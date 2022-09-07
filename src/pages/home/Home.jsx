63a111239ce65ed365da2358f6af6ff39dd5cb96
rons9n <rons9n@gmail.com>
2022-09-06 19:20:02 -0500

    Featured component styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Featured />
         </div>
     );
 };
