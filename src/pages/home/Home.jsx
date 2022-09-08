a8b0d119680773d5b9d5743d860be614f09eed4c
rons9n <rons9n@gmail.com>
2022-09-08 13:39:41 -0500

    featuredProperties component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Featured />
+            <FeaturedProperties />
         </div>
     );
 };
